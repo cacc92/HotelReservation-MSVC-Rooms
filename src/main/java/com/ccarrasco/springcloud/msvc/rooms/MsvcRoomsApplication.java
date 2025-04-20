6d6e92a814101bfdacd2877d6c5c67ba75f21a8d
Cesar Carrasco <ccarrascocarre@gmail.com>
2025-04-19 21:43:17 -0400

    Init Commit

---
+++ src/main/java/com/ccarrasco/springcloud/msvc/rooms/MsvcRoomsApplication.java
@@ -0,0 +1,13 @@
+package com.ccarrasco.springcloud.msvc.rooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcRoomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcRoomsApplication.class, args);
+	}
+
+}
